9fa193f252e80e92c7d77587a699ab077b763fd8
Délio <deliocqcosta@gmail.com>
2022-04-09 11:00:29 -0300

    Atualização aula do dia 09/04

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 //exercicio 1
 let nomeCompleto = "Délio Cornélio Queiroz da Costa";
 console.log(nomeCompleto);
@@ -46,9 +47,10 @@ for(i=0;i<=20;i++){
 function tabuada(numero){
     return numero*3;
 }
-for(i=1;i<=10;i++){
+for(let i=1;i<=10;i++){
     console.log(tabuada(i));
 }
+//especificar o "let" pra o contador do for para evitar lixo de memoria e travamento. Por padrão o sistema atribui var
 
 
 //exercicio 9
@@ -56,3 +58,61 @@ function converterEmHoras(minutos){
     return minutos/60;
 }
 console.log(converterEmHoras(150));
+
+
+const nome = "Délio";
+const idade = 27;
+const eCasado = true;
+
+const delio {
+    nome:"Délio",
+    idade : 27,
+    eCasado: false
+};  
+
+
+function tabuadaProf(numeral){
+    let result = [];
+    for(let i = 1; i<=10; i++){
+        //result.push(numeral + "x" + i + ":" + numeral*i);
+        result.push(`${numeral}x${i}: ${numeral*i}`);
+    }   
+    return result.join("\n");
+}
+
+console.log(tabuadaProf(7));
+
+
+const podeDigirirProf = (idade, voceBebeu = false)=> {
+    const isMaiorIdadeENaoBebeu = () => idade >= 18 && !voceBebeu;
+    
+    return `Você ${!isMaiorIdadeENaoBebeu() ? "não " : ""}pode dirigir!`;
+}
+
+console.log(podeDigirirProf(21, true));
+
+*/
+const menu = (option, label) =>{
+    let retorno;
+    switch (option){
+        case 0:
+        case 7:
+            retorno = "Sair do sistema";
+            break;
+        case 1:
+            return submenu();
+        default:
+            retorno = "Informe uma opção válida";
+    }
+    return retorno;
+};
+
+const submenu = (label) =>{
+    switch(label){
+        case "cliente":
+            return "Cadastrar cliente";
+        default:
+            return "Cadastrar professor";
+    }
+}
+console.log(menu(0, cliente));
